f39e1c76aea6bfb129a65faed9af7a25f95333a9
nash nakagawa <nash.nakagawa@gmail.com>
2016-04-22 03:44:39 -0400

    supports subdomain deletion

--- script/do-subdomain.sh
+++ script/do-subdomain.sh
@@ -3,40 +3,50 @@
 #API info here:
 #https://developers.digitalocean.com/#domains-list
 
-# $1 apikey
-# $2 newhost
-# $3 domain
-# $4 IP address of droplet
+# $1 action create | delete
+# $2 apikey
+# $3 newhost
+# $4 domain
+# $5 IP address of droplet
+
+#action
+if [ $1 == "create" ] ; then
+    action="POST"
+elif [ $1 == "delete" ] ; then
+    action="DELETE"
+else 
+    echo "invalid action [$1]: use create or delete"
+    exit 1;
+fi
 
 #do-apikey
-apikey = ${1:-"do-apikey"}
+apikey=${2:-"do-apikey"}
 
 #record to update
-record_id=${2:-"@"}
+record_id=${3:-"@"}
 
 #your domain ID
-domain_id=${3:-"nabelog.com"}
+domain_id=${4:-"nabelog.com"}
 
 #ip address
-ip=${4:-"188.166.177.243"}
+ip=${5:-"188.166.177.243"}
 
 ### Check if record_id already exists.
 
 # Get all records list and count how many same host name exists.
-records="$(curl -s -H "Authorization: Bearer $api_key" -H "Content-Type: application/json" \
-                 -d '{"type": "A", "name": "'"$record_id"'", "data": "'"$ip"'"}' \
-                 -X GET "https://api.digitalocean.com/v2/domains/$domain_id/records/" | jq '.domain_records | map(select(.name=="'"$record_id"'")) | length' )"
+record="$(curl -s -H "Authorization: Bearer $apikey" -H "Content-Type: application/json" \
+                 -X GET "https://api.digitalocean.com/v2/domains/$domain_id/records/" | jq '.domain_records | map(select(.name=="'"$record_id"'"))[] | .id' )"
 
-#echo "now: ${records} exists"
+#echo "GET: id ${record} "
 
-if [ `expr ${records}` -gt 0 ] ; then
-    echo "domain:${record_id} already exists"
-    exit 1;
-else
+#if [ `expr ${records}` -gt 0 ] ; then
+#    echo "domain:${record_id} already exists"
+#    exit 1;
+#else
     ### don't change ###
     #ip="$(curl  http://ipecho.net/plain)"
-    echo content="$(curl -s -H "Authorization: Bearer $api_key" -H "Content-Type: application/json" \
+    echo content="$(curl -s -H "Authorization: Bearer $apikey" -H "Content-Type: application/json" \
                  -d '{"type": "A", "name": "'"$record_id"'", "data": "'"$ip"'"}' \
-                 -X POST "https://api.digitalocean.com/v2/domains/$domain_id/records")"
-fi
+                 -X "$action" "https://api.digitalocean.com/v2/domains/$domain_id/records/$record")"
+#fi
 
